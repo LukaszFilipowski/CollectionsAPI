e1953c15cab3a2b2890389ef4d72af2e555b3cc8
Lukasz <Lukasz9999_12@o2.pl>
2017-11-26 16:11:05 +0100

    Token save to database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Token;
 
 class AuthController extends Controller
 {
     public function generateToken() 
     {
+        do {
+            $finded = false;
+            $token = str_random(16);
+            $tokenExists = Token::where('token', $token)->count();
+
+            if ($tokenExists == 0) {
+                $finded = true;
+                $t = new Token;
+                $t->token = $token;
+                $t->save();
+            }
+            
+        } while ($finded == false);
         
-        return str_random(16);
+        return $t;
     }
 }
